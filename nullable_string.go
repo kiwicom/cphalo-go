bd8d30d311dc436bf694579813fe6cf466698a1b
Matej Velikonja <matej@velikonja.si>
2018-11-23 09:55:29 +0100

    nullable string type

---
+++ nullable_string.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"bytes"
+	"strings"
+)
+
+// NullableString is a string, which marshalls empty string into null and vice versa.
+type NullableString string
+
+func (s NullableString) MarshalJSON() ([]byte, error) {
+	var buf bytes.Buffer
+	if len(string(s)) == 0 {
+		buf.WriteString(`null`)
+	} else {
+		buf.WriteString(`"` + string(s) + `"`)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (s *NullableString) UnmarshalJSON(in []byte) error {
+	str := string(in)
+	if str == `null` {
+		*s = ""
+		return nil
+	}
+
+	*s = NullableString(strings.Trim(str, `"`))
+
+	return nil
+}
---
+++ nullable_string_test.go
@@ -0,0 +1,59 @@
+package api
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+var (
+	tests []struct {
+		name string
+		ns   NullableString
+		s    string
+	}
+)
+
+func init() {
+	tests = []struct {
+		name string
+		ns   NullableString
+		s    string
+	}{
+		{"simple", NullableString("test"), `"test"`},
+		{"space", NullableString(" "), `" "`},
+		{"empty", NullableString(""), `null`},
+	}
+}
+
+func TestNullableString_MarshalJSON(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := json.Marshal(tt.ns)
+
+			if err != nil {
+				t.Fatalf("cannot marshall: %v", err)
+			}
+
+			got := string(b)
+			if got != tt.s {
+				t.Errorf("s %s; got %s", tt.s, string(got))
+			}
+		})
+	}
+}
+
+func TestNullableString_UnmarshalJSON(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NullableString("")
+
+			if err := json.Unmarshal([]byte(tt.s), &got); err != nil {
+				t.Fatalf("unmarshalling failed: %v", err)
+			}
+
+			if got != tt.ns {
+				t.Errorf("expected %s; got %s", tt.ns, string(got))
+			}
+		})
+	}
+}
