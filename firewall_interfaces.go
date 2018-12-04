186cf76e1b4b004d396b1d00772a69487ea99e12
Jan Gorjanc <johnee@gmail.com>
2018-12-04 12:45:37 +0100

    API client firewall interfaces

---
+++ firewall_interfaces.go
@@ -0,0 +1,95 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type FirewallInterface struct {
+	ID   string `json:"id,omitempty"`
+	URL  string `json:"url,omitempty"`
+	Name string `json:"name,omitempty"`
+}
+
+type ListFirewallInterfacesResponse struct {
+	Count      int                 `json:"count"`
+	Interfaces []FirewallInterface `json:"firewall_interfaces"`
+}
+
+type GetFirewallInterfaceResponse struct {
+	Interface FirewallInterface `json:"firewall_interface"`
+}
+
+type CreateFirewallInterfaceResponse = GetFirewallInterfaceResponse
+type CreateFirewallInterfaceRequest = GetFirewallInterfaceResponse
+type UpdateFirewallInterfaceRequest = GetFirewallInterfaceResponse
+
+func (c *Client) ListFirewallInterfaces() (response ListFirewallInterfacesResponse, err error) {
+	req, err := c.NewRequest(http.MethodGet, "firewall_interfaces", nil, nil)
+	if err != nil {
+		return response, fmt.Errorf("cannot create new request: %v", err)
+	}
+
+	_, err = c.Do(req, &response)
+	if err != nil {
+		return response, fmt.Errorf("cannot execute request: %v", err)
+	}
+
+	return response, nil
+}
+
+func (c *Client) GetFirewallInterface(ID string) (response GetFirewallInterfaceResponse, err error) {
+	req, err := c.NewRequest(http.MethodGet, "firewall_interfaces/"+ID, nil, nil)
+	if err != nil {
+		return response, fmt.Errorf("cannot create new request: %v", err)
+	}
+
+	_, err = c.Do(req, &response)
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
+
+func (c *Client) CreateFirewallInterface(fwInterface FirewallInterface) (response CreateFirewallInterfaceResponse, err error) {
+	req, err := c.NewRequest(http.MethodPost, "firewall_interfaces", nil, CreateFirewallInterfaceRequest{Interface: fwInterface})
+	if err != nil {
+		return response, fmt.Errorf("cannot create new create request: %v", err)
+	}
+
+	_, err = c.Do(req, &response)
+	if err != nil {
+		return response, fmt.Errorf("cannot execute create request: %v", err)
+	}
+
+	return response, nil
+}
+
+func (c *Client) UpdateFirewallInterface(fwInterface FirewallInterface) error {
+	req, err := c.NewRequest(http.MethodPut, "firewall_interfaces/"+fwInterface.ID, nil, UpdateFirewallInterfaceRequest{Interface: fwInterface})
+	if err != nil {
+		return fmt.Errorf("cannot create new update request: %v", err)
+	}
+
+	_, err = c.Do(req, nil)
+	if err != nil {
+		return fmt.Errorf("cannot execute update request: %v", err)
+	}
+
+	return nil
+}
+
+func (c *Client) DeleteFirewallInterface(ID string) error {
+	req, err := c.NewRequest(http.MethodDelete, "firewall_interfaces/"+ID, nil, nil)
+	if err != nil {
+		return fmt.Errorf("cannot create new delete request: %v", err)
+	}
+
+	_, err = c.Do(req, nil)
+	if err != nil {
+		return fmt.Errorf("cannot execute delete request: %v", err)
+	}
+
+	return nil
+}
---
+++ firewall_interfaces_test.go
@@ -0,0 +1,200 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestClient_ListFirewallInterfaces(t *testing.T) {
+	var err error
+	expectedResults := 7
+	expectedID := "eab260c09956012ee2db4087123ad87s"
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "firewall_interfaces_list", http.StatusOK),
+			t,
+			http.MethodGet,
+			"/v1/firewall_interfaces",
+			nil,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	resp, err := client.ListFirewallInterfaces()
+
+	if err != nil {
+		t.Fatalf("firewall policies list failed: %v", err)
+	}
+
+	if resp.Count != expectedResults {
+		t.Errorf("expected count to be %d; got %d", expectedResults, resp.Count)
+	}
+
+	if len(resp.Interfaces) != expectedResults {
+		t.Errorf("expected %d firewall policies; got %d", expectedResults, resp.Count)
+	}
+
+	if resp.Interfaces[0].ID != expectedID {
+		t.Errorf("expected firewall interface 0 to have ID %s; got %s", expectedID, resp.Interfaces[0].ID)
+	}
+}
+
+func TestClient_GetFirewallInterface(t *testing.T) {
+	var err error
+	expectedID := "eab41be09956012ee2db4087123ad87s"
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "firewall_interfaces_get", http.StatusOK),
+			t,
+			http.MethodGet,
+			"/v1/firewall_interfaces/id",
+			nil,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	resp, err := client.GetFirewallInterface("id")
+
+	if err != nil {
+		t.Fatalf("firewall policies get failed: %v", err)
+	}
+
+	if resp.Interface.ID != expectedID {
+		t.Errorf("expected firewall interface to have ID %s; got %s", expectedID, resp.Interface.ID)
+	}
+}
+
+func TestClient_CreateFirewallInterface(t *testing.T) {
+	var err error
+	reqBody := CreateFirewallInterfaceRequest{}
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "firewall_interfaces_get", http.StatusOK),
+			t,
+			http.MethodPost,
+			"/v1/firewall_interfaces",
+			&reqBody,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	policy := FirewallInterface{
+		ID:   "id",
+		Name: "hello",
+	}
+
+	resp, err := client.CreateFirewallInterface(policy)
+
+	if err != nil {
+		t.Fatalf("Firewall interface creating failed: %v", err)
+	}
+
+	expectedID := "eab41be09956012ee2db4087123ad87s"
+	if resp.Interface.ID != expectedID {
+		t.Errorf("expected response to containt ID=%s; got %s", expectedID, resp.Interface.ID)
+	}
+
+	if reqBody.Interface.ID != policy.ID {
+		t.Errorf("expected request to contain ID=%s; got %s", policy.ID, reqBody.Interface.ID)
+	}
+
+	if reqBody.Interface.Name != policy.Name {
+		t.Errorf("expected request to contain Name=%s; got %s", policy.Name, reqBody.Interface.Name)
+	}
+}
+
+func TestClient_UpdateFirewallInterface(t *testing.T) {
+	var err error
+	reqBody := UpdateFirewallInterfaceRequest{}
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "", http.StatusOK),
+			t,
+			http.MethodPut,
+			"/v1/firewall_interfaces/id",
+			&reqBody,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	policy := FirewallInterface{
+		ID:   "id",
+		Name: "hello",
+	}
+
+	err = client.UpdateFirewallInterface(policy)
+
+	if err != nil {
+		t.Fatalf("Firewall interface updating failed: %v", err)
+	}
+
+	if reqBody.Interface.ID != policy.ID {
+		t.Errorf("expected request to contain ID=%s; got %s", policy.ID, reqBody.Interface.ID)
+	}
+
+	if reqBody.Interface.Name != policy.Name {
+		t.Errorf("expected request to contain Name=%s; got %s", policy.Name, reqBody.Interface.Name)
+	}
+}
+
+func TestClient_DeleteFirewallInterface(t *testing.T) {
+	var err error
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "", http.StatusNoContent),
+			t,
+			http.MethodDelete,
+			"/v1/firewall_interfaces/id",
+			nil,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	err = client.DeleteFirewallInterface("id")
+
+	if err != nil {
+		t.Fatalf("Firewall interface deletion failed: %v", err)
+	}
+}
