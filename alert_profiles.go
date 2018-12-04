8db89d91b5896485150b8e621fbf741d49325384
Matej Velikonja <matej@velikonja.si>
2018-12-04 14:47:44 +0100

    alert profiles data source

---
+++ alert_profiles.go
@@ -0,0 +1,44 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type AlertProfile struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	GroupID     string    `json:"group_id"`
+	GroupName   string    `json:"group_name"`
+	Description string    `json:"description"`
+	Frequency   string    `json:"frequency"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	CreatedBy   string    `json:"created_by"`
+	UpdatedBy   string    `json:"updated_by"`
+	Shared      bool      `json:"shared"`
+	UsedBy      []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"used_by"`
+}
+
+type ListAlertProfilesResponse struct {
+	Count         int            `json:"count"`
+	AlertProfiles []AlertProfile `json:"alert_profiles"`
+}
+
+func (c *Client) ListAlertProfiles() (response ListAlertProfilesResponse, err error) {
+	req, err := c.NewRequest(http.MethodGet, "alert_profiles", nil, nil)
+	if err != nil {
+		return response, fmt.Errorf("cannot create new request: %v", err)
+	}
+
+	_, err = c.Do(req, &response)
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
---
+++ alert_profiles_test.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestClient_ListAlertProfiles(t *testing.T) {
+	var err error
+	expectedResults := 2
+	expectedID := "0226a27af95c11e5a92a471a4310f7c2"
+
+	ts := httptest.NewServer(
+		requestValidatorTestHandler(
+			jsonResponseTestHandler(t, "alert_profiles_list", http.StatusOK),
+			t,
+			http.MethodGet,
+			"/v1/alert_profiles",
+			nil,
+		),
+	)
+	defer ts.Close()
+
+	client := NewClient("", "")
+	client.BaseUrl, err = url.Parse(ts.URL)
+
+	if err != nil {
+		t.Fatalf("cannot parse url %s: %v", ts.URL, err)
+	}
+
+	resp, err := client.ListAlertProfiles()
+
+	if err != nil {
+		t.Fatalf("alert profiles list failed: %v", err)
+	}
+
+	if resp.Count != expectedResults {
+		t.Errorf("expected count to be %d; got %d", expectedResults, resp.Count)
+	}
+
+	if len(resp.AlertProfiles) != expectedResults {
+		t.Errorf("expected %d alert profiles; got %d", expectedResults, len(resp.AlertProfiles))
+	}
+
+	if resp.AlertProfiles[0].ID != expectedID {
+		t.Errorf("expected alert profile 0 to have ID %s; got %s", expectedID, resp.AlertProfiles[0].ID)
+	}
+}
